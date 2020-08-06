f9beedb68ad3ed4264172c71d26455425db007bd
corjohnson <cor.johnson@gmail.com>
2020-08-06 14:36:49 -0700

    Added view note layout

--- Android/app/src/main/java/co/miniforge/noto/MainActivity.kt
+++ Android/app/src/main/java/co/miniforge/noto/MainActivity.kt
@@ -2,8 +2,18 @@ package co.miniforge.noto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.miniforge.noto.data.Note
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
+
+    private val noteToView = Note(
+        id = 0,
+        title = "This is my note!",
+        content = "This is the content of my note!",
+        lastModified = LocalDateTime.now()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
